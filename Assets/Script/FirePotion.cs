ca2b56fdd1f096b968fe9e2c8846e6b23380129e
JoJorge <jorge13jorge13@gmail.com>
2016-11-13 16:06:41 +0800

    add FirePotion effect

--- Assets/Script/FirePotion.cs
+++ Assets/Script/FirePotion.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class FirePotion : Potion {
 
+    public Material lava;
+
 	// Use this for initialization
     protected override void Start () {
         color = Color.red;
 	}
+    public override void drink (Player player)
+    {
+        base.drink (player);
+        player.GetComponent<Renderer> ().material = lava;
+        player.GetComponent<ScrollingUVs_Layers> ().enabled = true;
+    }
 }
